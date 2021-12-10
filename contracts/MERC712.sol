11816f783bd569b139eb02bd70288c3e70db3e18
Nasim Akhtar <devnasim18@gmail.com>
2021-12-10 18:47:29 +0530

    added aux contract

--- contracts/MERC712.sol
+++ contracts/MERC712.sol
@@ -4,7 +4,7 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
-contract NFT is ERC721, AccessControl {
+contract MERC712 is ERC721, AccessControl {
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
